5a5a5072f274e372b478c34471b7b17241ae93f1
satlawa <p.satlawa@gmx.net>
2020-11-27 19:12:33 +0100

    initial commit

---
+++ build/thirdparty/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/philipp/Code/cpp/ucy_cpp_route_planner/thirdparty/googletest/googlemock
+# Build directory: /home/philipp/Code/cpp/ucy_cpp_route_planner/build/thirdparty/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/thirdparty/googletest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/philipp/Code/cpp/ucy_cpp_route_planner/thirdparty/googletest/googletest
+# Build directory: /home/philipp/Code/cpp/ucy_cpp_route_planner/build/thirdparty/googletest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
